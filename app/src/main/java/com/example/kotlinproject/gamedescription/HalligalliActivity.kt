dc0fd823710dc46c1b0ebf46679c48ba6951ae64
다영 <86220363+imdayoung@users.noreply.github.com>
2022-02-08 22:37:10 +0900

    Fifth commit

--- app/src/main/java/com/example/kotlinproject/gamedescription/HalligalliActivity.kt
+++ app/src/main/java/com/example/kotlinproject/gamedescription/HalligalliActivity.kt
@@ -1,14 +1,22 @@
 package com.example.kotlinproject.gamedescription
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.kotlinproject.R
+import com.example.kotlinproject.baseactivities.HomeActivity
+import kotlinx.android.synthetic.main.activity_halligalli.*
 
 class HalligalliActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_halligalli)
 
+        val goIntent = Intent(this, HomeActivity::class.java)
+
+        button_back.setOnClickListener {
+            startActivity(goIntent)
+        }
     }
 
 }
--- app/src/main/java/com/example/kotlinproject/gamedescription/RummikubActivity.kt
+++ app/src/main/java/com/example/kotlinproject/gamedescription/RummikubActivity.kt
@@ -1,12 +1,21 @@
 package com.example.kotlinproject.gamedescription
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.kotlinproject.R
+import com.example.kotlinproject.baseactivities.HomeActivity
+import kotlinx.android.synthetic.main.activity_rummikub.*
 
 class RummikubActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_rummikub)
+
+        val goIntent = Intent(this, HomeActivity::class.java)
+
+        button_back.setOnClickListener {
+            startActivity(goIntent)
+        }
     }
 }
